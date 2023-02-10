83bd53d6485e19af0f5b1b0ac0c7a646f4aa85e2
sean <sean.nuttall@gmail.com>
2023-02-10 15:09:29 -0800

    ES6 Coding Challenge

--- CodingChallenges/Feb6-Feb10/Pokescripts.js
+++ CodingChallenges/Feb6-Feb10/Pokescripts.js
@@ -4,8 +4,43 @@
     Description: Scripts for the Pokedex
 */
 
-function working() {
+let pokeName = "nidoking";
+let working = document.querySelector("#work");
+let fetching = document.querySelector("#fetching");
 
+working.addEventListener("click", workin);
+fetching.addEventListener("click", getStats);
+
+function getName(){
+
+    let name = document.getElementById("pokemonName").value;
+    let formattedName = formatName(name);
+    return(formattedName);
+
+}
+
+function formatName(name) {
+
+    name = name.toLowerCase();
+    name = name.replace(" ", "-");
+    return name;
+
+}
+
+async function getStats() {
+    
+    const find = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    const thisOne = await find.json();
+    document.getElementById("name").innerHTML = thisOne.name;
+    document.getElementById("hitPoints").innerHTML = "";
+    for(let stat of thisOne.stats) {
+        document.getElementById("hitPoints").innerHTML += `<p>${stat.stat.name}: ${stat.base_stat}</p>`; 
+    }
+}
+
+function workin() {
+
+    pokeName = getName();
     let flash = document.getElementById("waiting");
     
     flash.innerHTML = "Working, Please Wait";
